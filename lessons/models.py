efec68f49e90238b36739c8497231028b94cd637
thefallxo96 <titodelvalle824@gmail.com>
2026-02-01 09:05:29 -0500

    additions/fixes

--- lessons/models.py
+++ lessons/models.py
@@ -1,10 +1,19 @@
+from django.conf import settings
 from django.db import models
 
 class Lesson(models.Model):
-    title = models.CharField(max_length=120)
-    english = models.TextField()
-    spanish = models.TextField()
-    difficulty = models.IntegerField(default=1)
+    english_text = models.TextField()
+    spanish_text = models.TextField()
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="lessons",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.english_text[:50]
--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
-
-# Register your models here.
+# lessons/admin.py
+from .models import Lesson
+admin.site.register(Lesson)
